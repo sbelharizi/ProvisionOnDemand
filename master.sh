0ef0441fe3b6eb872dde627920a4be0e4f89a12d
jbohon-paloit <jbohon@palo-it.com>
2018-10-08 16:59:18 +0200

    create master script and begin jenkins in docker

---
+++ master.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source master.conf
+
+#Launch every services from master.conf in the same order
+for i in "${POD_SERVICES[@]}"
+do
+  CURRENT_SERVICE="$POD_PROVIDER/$POD_TYPE/$i/"
+  if [ -d $CURRENT_SERVICE ]; then
+    ./"$CURRENT_SERVICE/create.sh"
+  else
+    echo "service \"$i\" not found"
+  fi
+done
